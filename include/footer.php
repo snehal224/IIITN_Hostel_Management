3b6004e7f49b3d3b5ad070a65063ed7d5701ee67
Snehal <snehalbkale22@gmail.com>
2023-05-23 16:00:42 +0530

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - IIITN Hostel Management System - Developed by <a href="https://Netgoplus.com">IIITN STUDENTS</a>
+</footer>
